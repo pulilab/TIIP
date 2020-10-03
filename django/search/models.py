dbef1a617ca4beb787f0c0f504354a34c5c22478
Gyorgy Fekete <f@pulilab.com>
2020-10-03 16:42:59 +0200

    filter logic for sp and ps

--- django/search/models.py
+++ django/search/models.py
@@ -117,6 +117,10 @@ class ProjectSearch(ExtendedModel):
                     elif field == "portfolio":
                         lookup_param = "exact"
                         lookup = query_params.get(field)
+                    elif field in ["sp", "ps"]:
+                        lookup_param = "exact"
+                        lookup = query_params.get(field)
+                        queryset &= queryset.filter(project__review_states__approved=True)
 
                     queryset &= queryset.filter(**{"{}__{}".format(cls.FILTER_BY[field], lookup_param): lookup})
         return queryset
