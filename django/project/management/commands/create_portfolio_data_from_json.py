9d366f798b7ffb10404a785efa0efa5cb4584283
lordy <dk@pulilab.com>
2020-11-03 19:48:15 +0100

    Change donor to UNICEF

--- django/project/management/commands/create_portfolio_data_from_json.py
+++ django/project/management/commands/create_portfolio_data_from_json.py
@@ -23,7 +23,8 @@ class Command(BaseCommand, TestProjectData):
     def create_background_data(self, data_dict):
         pp.pprint('Creating background data if needed')
         self.org, _ = Organisation.objects.get_or_create(name=data_dict['organisation'])
-        self.d1, _ = Donor.objects.get_or_create(name='UNICEF', code='unicef')
+        self.d2, _ = Donor.objects.get_or_create(name=data_dict['d1'],
+                                                 code=data_dict['d1'].lower().replace(' ', '_'))
         self.d2, _ = Donor.objects.get_or_create(name=data_dict['d2'],
                                                  code=data_dict['d2'].lower().replace(' ', '_'))
         self.country, self.country_office = self.create_new_country_and_office(project_approval=False)
