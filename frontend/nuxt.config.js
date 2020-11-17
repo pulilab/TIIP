95726a795043461d3dfa2e86fa8c0fdd0735a291
Alonso Lamas <57102889+alpuli@users.noreply.github.com>
2020-11-17 12:55:08 +0100

    Fix map initiatives list and css fixes (#184)
    * Fix map initiatives list and css fixes
    
    * Remove console log

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -34,11 +34,15 @@ const loginUrl =
 
 const config = {
   head: {
-    title: 'INVENT',
+    title: "Invent: UNICEF's T4D and Innovation Inventory",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'INVENT' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: "Invent: UNICEF's T4D and Innovation Inventory",
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/ico', href: '/favicon.ico' },
