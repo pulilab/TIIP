209f8cc6e1fe2c4cd96fa7716e19176d8bfc3511
Alonso Lamas <al@pulilab.com>
2020-12-16 18:09:46 +0100

    Add table and columns filters new fields

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -79,6 +79,7 @@ const config = {
     { src: '~plugins/watchHead.js', ssr: false },
     { src: '~plugins/directives.js', ssr: false },
     { src: '~plugins/charts.js', ssr: false },
+    '~plugins/filters.js',
   ],
   modules: [
     '@nuxtjs/axios',
