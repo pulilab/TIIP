61baa0bf09a0779c708d28118817beef1cfdb002
Gyorgy Fekete <f@pulilab.com>
2020-12-16 11:25:28 +0100

    migrate project phases

--- django/project/utils.py
+++ django/project/utils.py
@@ -1,4 +1,8 @@
+from datetime import datetime
 from typing import Dict, List, Tuple, Union
+
+from django.conf import settings
+from .data_files.phase_mapping_table import ID_MAP
 
 
 def remove_keys(data_dict: Dict, keys: Union[List, Tuple]) -> Dict:
@@ -17,3 +21,13 @@ def _migrate_phases_to_stages(data: Dict, modified: datetime):
             }]
         data.pop('phase')
         return True
+
+
+def migrate_project_phases(instance: 'project.Project'):
+    if settings.MIGRATE_PHASES:
+        if not ID_MAP or not isinstance(ID_MAP, dict):  # pragma: no cover
+            print('ID MAPPING has not been completed, please run `manage.py fill_phase_mapping_table` first')
+        else:
+            if any([_migrate_phases_to_stages(instance.data, instance.modified), 
+                    _migrate_phases_to_stages(instance.draft, instance.modified)]):
+                instance.save(update_fields=['draft', 'data'])
