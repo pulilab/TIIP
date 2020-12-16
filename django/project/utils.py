8e86ecd5906af098c2f9981b4040200e006f8201
Gyorgy Fekete <f@pulilab.com>
2020-12-16 11:25:18 +0100

    migrate phases

--- django/project/utils.py
+++ django/project/utils.py
@@ -6,3 +6,14 @@ def remove_keys(data_dict: Dict, keys: Union[List, Tuple]) -> Dict:
         if key in data_dict:
             data_dict.pop(key, None)
     return data_dict
+
+
+def _migrate_phases_to_stages(data: Dict, modified: datetime):
+    if 'phase' in data and not data.get('stages'):
+        if str(data['phase']) in ID_MAP.keys():
+            data["stages"] = [{
+                "id": ID_MAP[str(data['phase'])],
+                "date": modified.isoformat().replace("+00:00", "Z"),
+            }]
+        data.pop('phase')
+        return True
