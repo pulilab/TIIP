857fefe9c4b70672b90caa089a754fbfbfd785ff
David-Puli <dk@pulilab.com>
2021-07-13 15:46:45 +0200

    Modified how Projects and ProjectPortfolioStates are being tracked through the system, search is still flaky.

--- django/search/views.py
+++ django/search/views.py
@@ -11,7 +11,7 @@ from rest_framework.viewsets import GenericViewSet
 
 from core.views import get_object_or_400, PortfolioAccessMixin
 from country.models import Donor, Country
-from project.models import Portfolio
+from project.models import Portfolio, ProjectPortfolioState
 from .serializers import MapResultSerializer, ListResultSerializer, PortfolioResultSerializer
 from .models import ProjectSearch
 
@@ -186,10 +186,13 @@ class SearchViewSet(PortfolioAccessMixin, mixins.ListModelMixin, GenericViewSet)
             query_params._mutable = True
             query_params.pop('ps', None)
             query_params.pop('sp', None)
+            reviews_alive = list(
+                ProjectPortfolioState.objects.filter(portfolio=portfolio_id).values_list('id', flat=True))
 
             if portfolio_page == "inventory":
-                qs = qs.exclude(project__review_states__portfolio_id=portfolio_id,
-                                project__review_states__is_active=True)
+                qs = qs.exclude(project__review_states__in=reviews_alive)
+                # qs = qs.exclude(project__review_states__portfolio_id=portfolio_id,
+                #                 project__review_states__is_active=True)
                 # edge case scenario where we need to ignore all the portfolio reliant query params from here
                 query_params.pop('portfolio', None)
             elif portfolio_page == "review":
