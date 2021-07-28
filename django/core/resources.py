c1cc2a407b03f27c3c7cbda57c62b7fdd6f37c0b
David-Puli <dk@pulilab.com>
2021-07-28 17:01:13 +0200

    Finished "serializer" to userprofiles

--- django/core/resources.py
+++ django/core/resources.py
@@ -1,18 +1,14 @@
 from import_export import resources
-from project.models import Project, UNICEFSector, UNICEFGoal, InnovationCategory, CPD, Stage, TechnologyPlatform, \
-    HardwarePlatform, NontechPlatform, ISC, PlatformFunction, UNICEFResultArea, InnovationWay
 from import_export.fields import Field
 from django.utils.translation import ugettext_lazy as _
 
-from country.models import Country, CountryOffice, Currency
-
-from project.serializers import LinkSerializer
+from user.models import UserProfile
+from django.contrib.auth.models import User
 
 
 class UserResource(resources.ModelResource):  # pragma: no cover
     """
     This class is basically a serializer for the django-import-export module
-    TODO: write unit tests if necessary
     Fields:
      - Stats:
        - last login
@@ -23,8 +19,6 @@ class UserResource(resources.ModelResource):  # pragma: no cover
        - Staff Status
        - Superuser Status
        - Global portfolio owner
-     - Groups
-       - groups
      - User Info
        - email address
        - name
@@ -34,11 +28,70 @@ class UserResource(resources.ModelResource):  # pragma: no cover
        - Donor
        - Language
     """
-    # General Overview
-    published = Field(column_name=_('Published?'))
-    contact = Field(column_name=_('Contact'))
-    team = Field(column_name=_('Team'))
-    overview = Field(column_name=_('Overview'))
-    narrative = Field(column_name=_('Narrative'))
+    # Stats
+    last_login = Field(column_name=_('Last login date'))
+    date_joined = Field(column_name=_('Date joined'))
+    is_odk_synced = Field(column_name=_('Synced to ODK?'))
+    # Permissions
+    is_active = Field(column_name=_('Active?'))
+    is_staff = Field(column_name=_('Staff member?'))
+    is_superuser = Field(column_name=_('Is superuser?'))
+    is_gpo = Field(column_name=_('Is Global Portfolio owner?'))
+    # User Info
+    email = Field(column_name=_('Email address'))
+    name = Field(column_name=_('Name'))
+    account_type = Field(column_name=_('Account type'))
+    organization = Field(column_name=_('Organization'))
     country = Field(column_name=_('Country'))
-    unicef_office = Field(column_name=_('UNICEF Office'))
+    donor = Field(column_name=_('Donor'))
+    language = Field(column_name=_('Language'))
+
+    class Meta:
+        model = User
+        fields = ('id', 'name', 'email', 'account_type', 'organization', 'country', 'donor', 'groups', 'language',
+                  'last_login', 'date_joined', 'is_odk_synced', 'is_active', 'is_staff', 'is_superuser', 'is_gpo')
+        export_order = ('id', 'name', 'email', 'account_type', 'organization', 'country', 'donor', 'groups', 'language',
+                        'last_login', 'date_joined', 'is_odk_synced', 'is_active', 'is_staff', 'is_superuser', 'is_gpo')
+
+    def dehydrate_last_login(self, user: User):
+        return user.last_login.date()
+
+    def dehydrate_date_joined(self, user: User):
+        return user.date_joined.date()
+
+    def dehydrate_is_odk_synced(self, user: User):
+        return user.userprofile.odk_sync
+
+    def dehydrate_is_active(self, user: User):
+        return user.is_active
+
+    def dehydrate_is_staff(self, user: User):
+        return user.is_staff
+
+    def dehydrate_is_superuser(self, user: User):
+        return user.is_superuser
+
+    def dehydrate_is_gpo(self, user: User):
+        return user.userprofile.global_portfolio_owner
+
+    def dehydrate_name(self, user: User):
+        return user.userprofile.name
+
+    def dehydrate_email(self, user: User):
+        return user.email
+
+    def dehydrate_account_type(self, user: User):
+        account_types = {x[0]: x[1] for x in UserProfile.ACCOUNT_TYPE_CHOICES}
+        return account_types[user.userprofile.account_type]
+
+    def dehydrate_organization(self, user: User):
+        return user.userprofile.organisation.name if user.userprofile.organisation else 'None'
+
+    def dehydrate_country(self, user: User):
+        return user.userprofile.country.name if user.userprofile.country else 'None'
+
+    def dehydrate_donor(self, user: User):
+        return user.userprofile.donor.name if user.userprofile.donor else 'None'
+
+    def dehydrate_language(self, user: User):
+        return user.userprofile.language
