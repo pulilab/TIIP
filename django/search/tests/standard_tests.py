6dc2f40d80bcf56ca2fa2cc22ad830ca8ca4f1b1
Gyorgy Fekete <f@pulilab.com>
2021-10-21 15:30:18 +0200

    remove query length check test

--- django/search/tests/standard_tests.py
+++ django/search/tests/standard_tests.py
@@ -141,13 +141,6 @@ class SearchTests(SetupTests):
         self.assertTrue(self.project2_id in response.json()['results']['found_in']['overview'])
         self.assertTrue(self.project_id in response.json()['results']['found_in']['overview'])
 
-    def test_query_length(self):
-        url = reverse("search-project-list")
-        data = {"q": "o"}
-        response = self.test_user_client.get(url, data, format="json")
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json(), ["Search term must be at least two characters long."])
-
     def test_filter_country(self):
         url = reverse("search-project-list")
         data = {"country": self.country_id}
